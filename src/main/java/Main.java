07f4565e3fd0042c0b80351602f25cd7173457e1
daniel-pohl <danielpohl@mail.de>
2024-05-03 15:32:29 +0200

    first Commit Daniel

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> zahlenListe = Arrays.asList(19, 31, 12, 3, 25, 7, 42);
+
+    // Step 1: Filter a list of numbers and only output the even numbers.
+        System.out.println("even numbers: ");
+        zahlenListe.stream()
+                    .filter(zahl -> zahl % 2 == 0)
+                    .forEach(System.out::println);
+
+
+
+    }
+}
